d03592df3848b70642ab4887843b4e77fc4edc8c
Adam Cox <adamcox.dev@gmail.com>
2022-08-19 12:25:40 -0600

    Added 2 more check for max length ammount and for special characters

--- password.js
+++ password.js
@@ -1,8 +1,11 @@
-let password = "OvechkinTheGreat8";
+let password = "OvechkinTheGreat8!";
 
 let minNumberOfLetters = /[a-z]/;
 let minNumberOfNumbers = /[0-9]/;
-if(password.length >= 10 && password.search(minNumberOfLetters) > 0 && password.search(minNumberOfNumbers) > 0){
+
+let specialCharacter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+if(password.length >= 10 && password.search(minNumberOfLetters) > 0 && password.search(minNumberOfNumbers) > 0 && password.length < 20 && password.search(specialCharacter) > 0){
     console.log('Password successful! Well done!')
 } else{
     console.log("You failed to enter a valid password. Please try again.")
